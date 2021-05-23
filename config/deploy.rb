f9958f91bc3a2dc2e3bb4a29817fc2d158edb4c3
Arjun Chib <arjunpatrickchib@gmail.com>
2021-05-22 22:00:02 -0400

    Add seed task

--- config/deploy.rb
+++ config/deploy.rb
@@ -66,3 +66,24 @@ set :puma_init_active_record, true
 set :puma_service_unit_env_vars, %w[
   DISCORD_APP=memebot
 ]
+
+namespace :deploy do
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+    
+  task :seed do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env)  do
+          execute :rake, 'db:seed'
+        end
+      end
+    end
+  end
+end
